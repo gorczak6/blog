6a26662365fc8ad2fddfaed0dd0253e15b6c6949
gorczak6@wp.pl <przemek@student.edu>
2017-08-01 21:41:49 +0200

    add gitignore file

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
-from datetime import timezone, datetime
-
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 
@@ -9,11 +9,11 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=256)
     text = models.TextField()
-    created_date = models.DateTimeField(default=datetime.now)
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_date = datetime.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
